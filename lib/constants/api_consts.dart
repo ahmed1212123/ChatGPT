f0510c5a2b423c6794a90aeaac182404a96c1581
ahmed1212123 <ahmedahly3453@gmail.com>
2023-04-17 03:21:40 +0200

    Initial commit

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,7 @@
+String BASE_URL = "https://api.openai.com/v1";
+//String API_KEY = "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";
+//String API_KEY = "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";
+//String API_KEY = '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';
+String API_KEY = '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';
+
+//
